04702b9af82e357365eb5d18f2308d89803509d2
Niten619 <nitenlama46@gmail.com>
2023-03-11 20:40:21 +0545

    added 'swap from sol' on-chain method

--- programs/dca-anchor/src/lib.rs
+++ programs/dca-anchor/src/lib.rs
@@ -1,6 +1,10 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{token::{SyncNative, sync_native, Token, TokenAccount, Transfer as Token_Transfer, transfer as token_transfer}, associated_token::AssociatedToken};
 use anchor_lang::system_program::{Transfer as Sol_Transfer, transfer as sol_transfer};
+use raydium_contract_instructions::amm_instruction;
+// use instructions::*;
+
+// pub mod instructions;
 
 declare_id!("FdHNRbJV4TypBrgjHhsB7EuUFScQ2SQnyTh8w3FQbMcg");
 
@@ -64,6 +68,80 @@ pub mod dca_anchor {
         sync_native(cpi_ctx_native)?;
         Ok(())
     }
+
+    // Function to initialize the dca account for the swapping process
+    pub fn initialize_dca(ctx: Context<InitializeDca>, start_time: u64, dca_amount: u64, dca_time: u64) -> Result<()> {
+        let dca_info = &mut ctx.accounts.dca_account;
+        dca_info.start_time = start_time;
+        dca_info.dca_amount = dca_amount;
+        dca_info.dca_time = dca_time;
+        dca_info.state = true;
+        Ok(())
+    }
+
+    // Function to swap token from sol (WrappedSol -> USDC)
+    // pub fn swap_from_sol(ctx: Context<ProxySwapBaseIn>, amount_in: u64, minimum_amount_out: u64) -> Result<()> {
+    //     msg!("Swap WrappedSol -> USDC");
+    //     // let dca_info = &mut ctx.accounts.dca_account;
+    //     // dca_info.start_time = dca_info.dca_time;
+        
+    //     // let amount_in = ctx.accounts.dca_account.dca_amount;
+    //     // let cpi_program = ctx.accounts.our_program_id.to_account_info();
+    //     instructions::swap_base_in(ctx, amount_in, minimum_amount_out)?;
+    //     msg!("Successfully Swapped");
+    //     Ok(())
+    // }
+
+    // Function to swap token from sol (WrappedSol -> USDC)
+    pub fn swap_from_sol(
+        ctx: Context<SwapBaseIn>,
+        amount_in: u64,
+        minimum_amount_out: u64,
+    ) -> Result<()> {
+        let ix = amm_instruction::swap_base_in(
+            &ctx.program_id.key(),
+            ctx.accounts.amm.key,
+            ctx.accounts.amm_authority.key,
+            ctx.accounts.amm_open_orders.key,
+            ctx.accounts.amm_target_orders.key,
+            ctx.accounts.pool_coin_token_account.key,
+            ctx.accounts.pool_pc_token_account.key,
+            ctx.accounts.serum_program.key,
+            ctx.accounts.serum_market.key,
+            ctx.accounts.serum_bids.key,
+            ctx.accounts.serum_asks.key,
+            ctx.accounts.serum_event_queue.key,
+            ctx.accounts.serum_coin_vault_account.key,
+            ctx.accounts.serum_pc_vault_account.key,
+            ctx.accounts.serum_vault_signer.key,
+            ctx.accounts.user_source_token_account.key,
+            ctx.accounts.user_destination_token_account.key,
+            ctx.accounts.user_source_owner.key,
+            amount_in,
+            minimum_amount_out,
+        )?;
+
+        let (_vault_address,bump_seed)=Pubkey::find_program_address(
+            &[
+                &ctx.accounts.sender_account.key.to_bytes(),
+                &ctx.accounts.dca_account.key().to_bytes()
+            ],
+            ctx.program_id,
+        );
+        let vault_signer_seeds: &[&[&[_]]] = &[&[
+            &ctx.accounts.sender_account.key.to_bytes(),
+            &ctx.accounts.dca_account.key().to_bytes(),
+            &[bump_seed],
+        ]];
+
+        solana_program::program::invoke_signed(
+            &ix,
+            &ToAccountInfos::to_account_infos(ctx.accounts),
+            &vault_signer_seeds,
+        )?;
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
@@ -116,6 +194,146 @@ pub struct DepositSol<'info> {
     #[account(mut)]
     pub vault_wrap_ata: Account<'info, TokenAccount>,
     pub associated_token_program: Program<'info, AssociatedToken>
+}
+
+#[derive(Accounts)]
+pub struct InitializeDca<'info> {
+    #[account(mut)]
+    pub dca_account: Account<'info, DcaData>
+}
+
+// #[derive(Accounts)]
+// pub struct SwapFromSol<'info> {
+//     // CHECK:
+//     // #[account(constraint = program.programdata_address()? == Some(program_data.key()))]
+//     // #[account(mut)]
+//     // pub our_program_id: AccountInfo<'info>,
+
+//     /// CHECK: Safe
+//     pub our_program_id: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub amm_id: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     pub amm_authority: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub amm_open_orders: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub amm_target_orders: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub pool_coin_token_account: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub pool_pc_token_account: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     pub serum_program_id: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub serum_market: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub serum_bids: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub serum_asks: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub serum_event_queue: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub serum_coin_vault_account: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     #[account(mut)]
+//     pub serum_pc_vault_account: AccountInfo<'info>,
+//     /// CHECK: Safe
+//     pub serum_vault_signer: AccountInfo<'info>,
+//     /// CHECK:
+//     #[account(mut, seeds = [sender_account.key().as_ref(), dca_account.key().as_ref()], bump)]
+//     pub vault: AccountInfo<'info>,
+//     #[account(mut)]
+//     pub vault_wrap_ata: Account<'info, TokenAccount>,
+//     #[account(mut)]
+//     pub vault_token_ata: Account<'info, TokenAccount>, // This is the user destination token account
+//     /// CHECK:
+//     #[account(mut)]
+//     pub token_mint: AccountInfo<'info>,
+//     /// CHECK:
+//     #[account(mut)]
+//     pub sender_account: AccountInfo<'info>,
+//     #[account(mut)]
+//     pub dca_account: Account<'info, DcaData>,
+//     /// CHECK:
+//     #[account(mut)]
+//     pub token_mint_wrap: AccountInfo<'info>,
+//     pub token_program: Program<'info, Token>
+// }
+
+
+/// Accounts for an `swap_base_in` instruction.
+#[derive(Accounts)]
+pub struct SwapBaseIn<'info> {
+    /// CHECK: Safe. amm Account
+    #[account(mut)]
+    pub amm: AccountInfo<'info>,
+    /// CHECK: Safe. Amm authority Account
+    pub amm_authority: AccountInfo<'info>,
+    /// CHECK: Safe. amm open_orders Account
+    #[account(mut)]
+    pub amm_open_orders: AccountInfo<'info>,
+    /// CHECK: Safe. amm target_orders Account
+    #[account(mut)]
+    pub amm_target_orders: AccountInfo<'info>,
+    /// CHECK: Safe. pool_token_coin Amm Account to swap FROM or To,
+    #[account(mut)]
+    pub pool_coin_token_account: AccountInfo<'info>,
+    /// CHECK: Safe. pool_token_pc Amm Account to swap FROM or To,
+    #[account(mut)]
+    pub pool_pc_token_account: AccountInfo<'info>,
+    /// CHECK: Safe. serum dex program id
+    pub serum_program: AccountInfo<'info>,
+    /// CHECK: Safe. serum market Account. serum_dex program is the owner.
+    #[account(mut)]
+    pub serum_market: AccountInfo<'info>,
+    /// CHECK: Safe. bids Account
+    #[account(mut)]
+    pub serum_bids: AccountInfo<'info>,
+    /// CHECK: Safe. asks Account
+    #[account(mut)]
+    pub serum_asks: AccountInfo<'info>,
+    /// CHECK: Safe. event_q Account
+    #[account(mut)]
+    pub serum_event_queue: AccountInfo<'info>,
+    /// CHECK: Safe. coin_vault Account
+    #[account(mut)]
+    pub serum_coin_vault_account: AccountInfo<'info>,
+    /// CHECK: Safe. pc_vault Account
+    #[account(mut)]
+    pub serum_pc_vault_account: AccountInfo<'info>,
+    /// CHECK: Safe. vault_signer Account
+    #[account(mut)]
+    pub serum_vault_signer: AccountInfo<'info>,
+    /// CHECK: Safe. user source token Account. user Account to swap from.
+    #[account(mut)]
+    pub user_source_token_account: AccountInfo<'info>,
+    /// CHECK: Safe. user destination token Account. user Account to swap to.
+    #[account(mut)]
+    pub user_destination_token_account: AccountInfo<'info>,
+    /// CHECK: Safe. user owner Account
+    // #[account(signer)]
+    #[account(mut, seeds = [sender_account.key().as_ref(), dca_account.key().as_ref()], bump)]
+    pub user_source_owner: AccountInfo<'info>,
+    /// CHECK: Safe. The spl token program
+    #[account(address = spl_token::ID)]
+    pub spl_token_program: AccountInfo<'info>,
+
+    #[account(init_if_needed, payer = sender_account, space = 8 + 8 + 32 + 32 + 8 + 8 + 8 + 1 + 1 + 8)]
+    pub dca_account: Account<'info, DcaData>,
+    #[account(mut)]
+    pub sender_account: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
